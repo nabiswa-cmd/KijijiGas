51c7f0f9d50d697cf72ee52917485f00b60285ce
Nabiswa Jamea <nabiswaj8@gmail.com>
2025-12-14 21:21:51 +0300

    Fix start.sh for production

--- start.sh
+++ start.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
-# Install dependencies
+
 pip install -r requirements.txt
 
-# Apply database migrations
-python KIJIJIGAS/kijijiGas/manage.py migrate
+python manage.py migrate
+python manage.py collectstatic --noinput
 
-# Collect static files (optional, needed for static assets)
-python KIJIJIGAS/kijijiGas/manage.py collectstatic --noinput
-
-# Run the Django server
-python KIJIJIGAS/kijijiGas/manage.py runserver 0.0.0.0:8000
-
+gunicorn kijijiGas.wsgi:application --bind 0.0.0.0:$PORT
